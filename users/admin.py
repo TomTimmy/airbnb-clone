5c3c865fceefe7107850086e40526ce936e7643c
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2021-12-23 11:57:03 +0900

    #3.4 Falling in Love with Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from . import models  # ? from . 뜻: admin.py 즉, 지금 이 파일이 있는 폴더에서...
 
 
-@admin.register(models.User)
+@admin.register(models.User)  # ! Decorator 는 이렇게 calss 위에 써야 작동한다.
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """Custom User Admin"""
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = ("superhost", "language")
+
+
+# admin.site.register(models.User, CustomUserAdmin)
